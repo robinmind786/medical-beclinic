b3edb8aee0423b57641e5cbe6adbebce4d878dd5
Robin Mind <43693130+robinmind786@users.noreply.github.com>
2021-07-31 21:50:05 +0600

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,33 +1,29 @@
-// Mobile Menu
+const openNav = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const closeNav = document.querySelector('.close');
 
-// const navLinks = document.querySelector('.nav-links')
-// const openNav = document.querySelector('.topnav .burger');
-// const closeNav = document.querySelector('.topnav .fa-remove');
+openNav.addEventListener('click', function(){
+	navLinks.style.left = "0";
+});
 
-// openNav.addEventListener('click', function(){
-//   navLinks.style.left = '0';
-// });
-
-// closeNav.addEventListener('click', function(){
-//   navLinks.style.left = '-100%';
-// });
-
-// console.log(closeNav)
+closeNav.addEventListener('click', function(){
+  navLinks.style.left = '-100%';
+});
 
 
-// const hasCollapsible = document.querySelectorAll(".has-collapsible");
+const hasCollapsible = document.querySelectorAll(".has-collapsible");
 
-// hasCollapsible.forEach(function (collapsible) {
-//   collapsible.addEventListener("click", function () {
-//     collapsible.classList.toggle("active");
+hasCollapsible.forEach(function (collapsible) {
+  collapsible.addEventListener("click", function () {
+    collapsible.classList.toggle("active");
 
-//     hasCollapsible.forEach(function (otherCollapsible) {
-//       if (otherCollapsible !== collapsible) {
-//         otherCollapsible.classList.remove("active");
-//       }
-//     });
-//   });
-// });
+    hasCollapsible.forEach(function (otherCollapsible) {
+      if (otherCollapsible !== collapsible) {
+        otherCollapsible.classList.remove("active");
+      }
+    });
+  });
+});
 
 
 
